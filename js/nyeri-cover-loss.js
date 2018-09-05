ad8775887dcd90e748d388977aa8061bfab9fdb7
WahomeEmmanuel <gathage.ew@gmail.com>
2018-09-05 12:07:11 +0300

    Update hackerthon code

--- js/nyeri-cover-loss.js
+++ js/nyeri-cover-loss.js
@@ -1,36 +1,39 @@
 google.charts.load('current', {'packages':['bar']});
-google.charts.setOnLoadCallback(drawChart2);
-function drawChart2(){
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
     var data = google.visualization.arrayToDataTable([
         ['Year', 'annual tree cover loss'],
-        ['2001', 252],
-        ['2002', 77],
-        ['2003', 512],
-        ['2004', 27],
-        ['2005', 59],
-        ['2006', 13],
-        ['2007', 318],
-        ['2008', 58],
-        ['2009', 89],
-        ['2010', 291],
-        ['2011', 312],
-        ['2012', 274],
-        ['2013', 299],
-        ['2014', 272],
-        ['2015', 429],
-        ['2016', 688]
+        ['2001', 19027],
+        ['2002', 20288],
+        ['2003', 13553],
+        ['2004', 16998],
+        ['2005', 12745],
+        ['2006', 20656],
+        ['2007', 26103],
+        ['2008', 19224],
+        ['2009', 17696],
+        ['2010', 22693],
+        ['2011', 17300],
+        ['2012', 17899],
+        ['2013', 13564],
+        ['2014', 15357],
+        ['2015', 15616],
+        ['2016', 19048]
     ]);
+
     var options = {
         chart: {
-            'legend':'left',
-            'is3D':true,
-            title: 'TREE COVER LOSS IN NYERI: 2000 - 2016',
-            subtitle: 'From 2001 to 2016, Nyeri lost 3.96kha of tree cover, equivalent to a 2.3% decrease since 2000 and 540kt of CO₂ emissions.',
+            title: '',
+            subtitle: '',
         }
     };
 
-    var chart = new google.charts.Bar(document.getElementById('drawChart2_div'));
+    var chart = new google.charts.Bar(document.getElementById('columnchartMaterial'));
 
     chart.draw(data, google.charts.Bar.convertOptions(options));
-
 }
+
+$(window).resize(function () {
+    drawChart();
+});
